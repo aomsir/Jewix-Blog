0c4c7c71a918f2bc8f0fa687f71be948fd4fea4e
aomsir <info@say521.cn>
2022-12-29 13:36:14 +0800

    初始化项目

---
+++ src/main/java/com/aomsir/jewixapi/JewixApiApplication.java
@@ -0,0 +1,13 @@
+package com.aomsir.jewixapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JewixApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JewixApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aomsir/jewixapi/JewixApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aomsir.jewixapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JewixApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
