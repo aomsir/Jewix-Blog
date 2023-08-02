d6128653f35dfacd382039e75442de4a857d01d5
Polarish <72297937+AllianceTing@users.noreply.github.com>
2023-08-02 22:20:56 +0800

    feat: Update ExceptionConstants.java

--- Jewix-API/src/main/java/com/aomsir/jewixapi/constant/ExceptionConstants.java
+++ Jewix-API/src/main/java/com/aomsir/jewixapi/constant/ExceptionConstants.java
@@ -11,6 +11,8 @@ public interface ExceptionConstants {
     String DATABASE_EXCEPTION = "数据库异常,请联系管理员";
 
     String NONE_EXCEPTION = "未知异常,请刷新重试";
+    
     String OVER_FLOW = "溢出异常";
+    
     String OUT_OF_BOUNDS = "数组越界";
 }
